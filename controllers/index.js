ca46f350a59d493e6b48472c9a86b78d33a9d979
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2023-05-13 18:41:12 -0400

    set up for deployment

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const users = require('./users');
+const pages = require('./pages');
 
 // localhost:3001/
+router.use('/', pages);
+// localhost:3001/users
 router.use('/users', users);
  
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const controllers = require('./controllers');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
 
+// we need this to deploy to the server so it can use the port that heroku gives us
 const PORT = process.env.PORT || 3001;
 const app = express();
 // build tables based on models
 const models = require('./models');
+
+// to access the public folder
+app.use(express.static('public'));
+
+// set up handlebars
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // necessary for post requests
 app.use(express.json());
