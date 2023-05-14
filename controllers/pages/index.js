1998f0c4ced85dc7fb68b165d5cded134ec899d5
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2023-05-13 21:39:24 -0400

    dashboard and return user data

--- controllers/pages/index.js
+++ controllers/pages/index.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
+const {User} = require('../../models');
 
 router.get('/', (req, res) => { 
     res.render('reviews');
- });
+});
+ 
+// we dont know how long it will take to get the data from the database so we use async
+router.get('/dashboard/:user_id', async (req, res) => { 
+    // find by primary key
+    const userData = await User.findByPk(req.params.user_id, {
+        attributes: { exclude: ['password'] }
+    });
+    console.log('sequelize data : ', userData);
+
+    const actualUserData = userData.get({ plain: true });
+    console.log('actual data : ', actualUserData);
+    // pass data to handlebars to display it
+    res.render('dashboard', actualUserData);
+ })
 
 module.exports = router;
